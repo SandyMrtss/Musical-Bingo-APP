d35740d7fe8283dabe7b43d05d63be9cf066162e
Sandy Martos <bebesubi2@gmail.com>
2024-08-13 23:13:03 +0200

    Adds Game, Song and Ranking model and mappers. Modifies player domain to adapt changes

---
+++ src/main/java/com/sandy/bingo/model/mapper/SongDTOMapper.java
@@ -0,0 +1,12 @@
+package com.sandy.bingo.model.mapper;
+
+import com.sandy.bingo.model.domain.Game;
+import com.sandy.bingo.model.domain.Song;
+import com.sandy.bingo.model.dto.SongDTO;
+import com.sandy.bingo.model.dto.request.SongDTORequest;
+
+public interface SongDTOMapper {
+    SongDTO map(Song song);
+    Song map(SongDTO songDTO, Game game);
+    Song map(SongDTORequest songDTORequest, Game game);
+}
