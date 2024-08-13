68306dad5d969928f1bbee711e7df115f6c80943
Sandy Martos <bebesubi2@gmail.com>
2024-08-13 23:14:36 +0200

    Implements Song CRUD

--- src/main/java/com/sandy/bingo/repository/SongRepository.java
+++ src/main/java/com/sandy/bingo/repository/SongRepository.java
@@ -1,9 +1,13 @@
 package com.sandy.bingo.repository;
 
+import com.sandy.bingo.model.domain.Game;
 import com.sandy.bingo.model.domain.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface SongRepository extends JpaRepository<Song, Integer> {
-//}
+import java.util.List;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Integer> {
+    List<Song> findByGame(Game game);
+}
